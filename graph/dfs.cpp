6e06b731e6564ad4715948d90f90869416c1d15d
aryaman-agar <aryaman2212@gmail.com>
2025-01-02 23:19:55 +0530

    Update dfs.cpp

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -35,34 +35,3 @@ class Solution {
     }
 };
 
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
-        vector<vector<int>> adj(
-            V); // Use vector of vectors instead of array of vectors.
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        Solution obj;
-        vector<int> ans = obj.dfsOfGraph(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
